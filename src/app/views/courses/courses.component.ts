7408986555c6a6e68463ed44bb333f7835d68d2e
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2023-02-11 15:25:19 -0500

    removed home component async

--- src/app/views/courses/courses.component.ts
+++ src/app/views/courses/courses.component.ts
@@ -1,18 +1,17 @@
-import { Component } from '@angular/core';
-import { Course } from '../types/interfaces/course.interface';
-import { CouponCodeService } from './services/coupon-code/coupon-code.service';
+import { Component } from "@angular/core";
+import { Course } from "../types/interfaces/course.interface";
+import { CouponCodeService } from "./services/coupon-code/coupon-code.service";
 
 @Component({
-  selector: 'app-courses',
-  templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  selector: "app-courses",
+  templateUrl: "./courses.component.html",
+  styleUrls: ["./courses.component.scss"],
 })
 export class CoursesComponent {
   public courses: Course[] = [];
-  public currentMonthsCode = this.couponCodeService.currentCode;
+  // public currentMonthsCode = this.couponCodeService.currentCode;
 
   constructor(private couponCodeService: CouponCodeService) {
     this.courses = this.couponCodeService.courses;
   }
-
 }
