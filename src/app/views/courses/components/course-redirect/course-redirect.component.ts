2d084e13ed2182b9cf321eb3af8155fa56f0e5ad
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2023-02-12 01:46:55 -0500

    added ActivatedRoute

--- src/app/views/courses/components/course-redirect/course-redirect.component.ts
+++ src/app/views/courses/components/course-redirect/course-redirect.component.ts
@@ -7,44 +7,68 @@ import { CouponCodeService } from "../../services/coupon-code/coupon-code.servic
   templateUrl: "./course-redirect.component.html",
   styleUrls: ["./course-redirect.component.scss"],
 })
-export class CourseRedirectComponent implements OnInit {
-  public courses: { courseKey: string; urlTitle: string }[] = [];
-
-  constructor(
-    private route: ActivatedRoute,
-    private couponCodeService: CouponCodeService
-  ) {
-    this.courses = this.couponCodeService.courses;
-  }
-
-  public ngOnInit() {
-    this.init();
-  }
-
-  private init() {
-    this.route.root.queryParams.subscribe((params) => {
-      const courseQueryString = params.course;
-
-      if (courseQueryString) {
-        this.redirectIfValidCourse(courseQueryString);
-      }
-    });
-  }
-
-  public redirectIfValidCourse(courseQueryString: string): void {
-    for (const course of this.couponCodeService.courses) {
-      if (course.courseKey === courseQueryString) {
-        this.redirectByCourse(course);
-        break;
-      }
-    }
-  }
-
-  private redirectByCourse(course: {
+export class CourseRedirectComponent {
+  public courses: {
     courseKey: string;
     urlTitle: string;
-    url?: string;
-  }) {
-    window.location.href = course.url;
+    thumbnailUrl: string;
+  }[] = [];
+
+  constructor(
+    private route: ActivatedRoute // private couponCodeService: CouponCodeService
+  ) {
+    this.courses = [
+      {
+        courseKey: "100-angular",
+        urlTitle: "100-angular-challenge",
+        thumbnailUrl: "/assets/100-angular.jpg",
+      },
+      {
+        courseKey: "fe-interview",
+        urlTitle: "100-front-end-interview-questions-challenge",
+        thumbnailUrl: "/assets/front-end-course.jpg",
+      },
+      {
+        courseKey: "100-algorithms",
+        urlTitle: "100-algorithms-challenge",
+        thumbnailUrl: "/assets/100-algorithms.jpg",
+      },
+      {
+        courseKey: "javascript",
+        urlTitle: "javascript-360",
+        thumbnailUrl: "/assets/javascript-course.jpg",
+      },
+    ];
   }
+
+  // public ngOnInit() {
+  //   this.init();
+  // }
+
+  // private init() {
+  //   this.route.root.queryParams.subscribe((params) => {
+  //     const courseQueryString = params.course;
+
+  //     if (courseQueryString) {
+  //       this.redirectIfValidCourse(courseQueryString);
+  //     }
+  //   });
+  // }
+
+  // public redirectIfValidCourse(courseQueryString: string): void {
+  //   for (const course of this.couponCodeService.courses) {
+  //     if (course.courseKey === courseQueryString) {
+  //       this.redirectByCourse(course);
+  //       break;
+  //     }
+  //   }
+  // }
+
+  // private redirectByCourse(course: {
+  //   courseKey: string;
+  //   urlTitle: string;
+  //   url?: string;
+  // }) {
+  //   window.location.href = course.url;
+  // }
 }
