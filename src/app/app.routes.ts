0d2432383fb1caffc215c3b892c53623d16c16df
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2023-02-23 01:21:54 -0500

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,51 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './views/home/home.component';
-import { CoursesComponent } from './views/courses/courses.component';
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./views/home/home.component";
+import { CoursesComponent } from "./views/courses/courses.component";
 
 export const APP_ROUTES: Routes = [
   {
-    path: '',
-    component: HomeComponent
+    path: "",
+    component: HomeComponent,
+  },
+  // {
+  //   path: 'courses',
+  //   component: CoursesComponent
+  // }
+
+  {
+    path: "",
   },
   {
-    path: 'courses',
-    component: CoursesComponent
-  }
+    path: "",
+  },
+  {
+    path: "",
+  },
+  {
+    path: "",
+  },
+  {
+    path: "",
+  },
+  {
+    path: "",
+  },
+  {
+    path: "",
+  },
+  {
+    path: "",
+  },
+  {
+    path: "",
+  },
+  {
+    path: "",
+  },
+  {
+    path: "",
+  },
+  {
+    path: "",
+  },
 ];
