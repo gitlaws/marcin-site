2df2d5f0398be85f84eaec86160beac0d8c84871
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2023-02-28 06:04:20 -0500

    changes

--- src/app/views/home/components/email-signup/email-signup.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-email-signup',
-  templateUrl: './email-signup.component.html',
-  styleUrls: ['./email-signup.component.scss']
-})
-export class EmailSignupComponent {}
--- src/app/views/types/interfaces/course.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface Course {
-  courseKey: string;
-  urlTitle: string;
-  url?: string;
-  thumbnailUrl: string;
-}
